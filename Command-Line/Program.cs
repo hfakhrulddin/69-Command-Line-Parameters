341b7ba37aa75ce2056ec45a3370a9f39223ad03
Hussen Fakhrulddin <hf556482@gmail.com>
2016-07-13 23:10:22 -0700

    init

---
+++ Command-Line/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Command_Line
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            args = new string[] { "I am 1st", "I am 2nd", "I am 3rd"}; 
+
+            Console.WriteLine("Number of command line parameters = {0}", args.Length);
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine("Arg[{0}] = [{1}]", i, args[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
